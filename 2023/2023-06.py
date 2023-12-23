cd89349cafeb68367c5ce17c3a2e2f1d28e2c8cb
Cristina Marques <ccmarques98@gmail.com>
2023-12-23 22:51:49 +0100

    2023 day 6

--- 2023/2023-06.py
+++ 2023/2023-06.py
@@ -2,14 +2,30 @@ from aocd import get_data
 
 input = get_data(day=6, year=2023).strip().split("\n")
 # input = open("test.txt").read().strip().split("\n")
-# ['Time:        38     94     79     70', 'Distance:   241   1549   1074   1091']
 
-times = [int(x) for x in input[0].split(": ")[1].split()]
-distances = [int(x) for x in input[1].split(": ")[1].split()]
-
-races = [(times[i], distances[i]) for i in range(len(times))]
 
 print("\n## Part 1 ##")
 
-print(races)
+times, distances = [list(map(int, x.split(": ")[1].split())) for x in input]
+n = 1
 
+for time, dist in zip(times, distances):
+    margin = 0
+    for hold in range(1, time):
+        if hold * (time - hold) > dist:
+            margin += 1
+    n *= margin
+
+print(n)
+
+
+print("\n## Part 2 ##")
+
+time = int("".join(input[0].split(": ")[1].split()))
+dist = int("".join(input[1].split(": ")[1].split()))
+
+margin = 0
+for hold in range(1, time):
+    if hold * (time - hold) > dist:
+        margin += 1
+print(margin)
