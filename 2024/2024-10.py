5f5a7e3d6e56a11e64f35af9e2bae39a3ce860b2
randmon <ccmarques98@gmail.com>
2024-12-22 13:57:47 +0100

    day 10 part 2

--- 2024/2024-10.py
+++ 2024/2024-10.py
@@ -2,7 +2,7 @@ from aocd import get_data
 from utils import bcolors
 from collections import deque
 
-is_example = True
+is_example = False
 
 if not is_example: input = get_data(day=10, year=2024).strip().split("\n")
 else: input = open("2024/10.in").read().strip().split("\n")
@@ -16,7 +16,7 @@ height = len(input)
 
 trail_heads = [(row, col) for row in range(height) for col in range(width) if input[row][col] == 0]
 
-def score(grid, row, col):
+def score_1(grid, row, col):
   assert grid[row][col] == 0
   path = deque([(row, col)])
   seen = {(row, col)}
@@ -43,5 +43,42 @@ def score(grid, row, col):
       
   return summits
   
-result = sum([score(input, row, col) for row, col in trail_heads])
+result = sum([score_1(input, row, col) for row, col in trail_heads])
+print(result)
+
+
+print("\n## Part 2 ##")
+
+
+def score_2(grid, row, col):
+  assert grid[row][col] == 0
+  path = deque([(row, col)])
+  seen = {(row, col): 1}
+  trails = 0
+  
+  while len(path) > 0:
+    curr_row, curr_col = path.popleft()
+    if grid[curr_row][curr_col] == 9: trails += seen[(curr_row, curr_col)]
+    
+    for nr, nc in [(curr_row-1, curr_col), 
+                   (curr_row+1, curr_col), 
+                   (curr_row, curr_col-1), 
+                   (curr_row, curr_col+1)]:
+      
+      # If out of bounds, skip
+      if nr < 0 or nr >= height or nc < 0 or nc >= width: continue
+      # If not a +1 step, skip
+      if grid[nr][nc] != grid[curr_row][curr_col] + 1: continue
+      # If already seen, add count from previous cell
+      if (nr, nc) in seen:
+        seen[(nr, nc)] += seen[(curr_row, curr_col)]
+        continue
+      seen[(nr, nc)] = seen[(curr_row, curr_col)]
+      
+      # Add next cell to path
+      path.append((nr, nc))
+      
+  return trails
+
+result = sum([score_2(input, row, col) for row, col in trail_heads])
 print(result)
